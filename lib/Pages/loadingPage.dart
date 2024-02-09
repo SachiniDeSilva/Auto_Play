b0d5b09bd19da5e723641e172a6e9124e012d028
Williyam Silvage Sachini Dishala <90495152+desi98@users.noreply.github.com>
2024-02-09 22:24:53 +0530

    create loading page

---
+++ lib/Pages/loadingPage.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class _Loading extends StatefulWidget {
+  const _Loading({super.key});
+
+  @override
+  State<_Loading> createState() => __LoadingState();
+}
+
+class __LoadingState extends State<_Loading> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
