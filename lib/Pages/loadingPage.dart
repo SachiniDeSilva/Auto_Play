96bbfed4505ce0557b821fe86fd8bd35919c2677
Williyam Silvage Sachini Dishala De Silva <90495152+SachiniDeSilva@users.noreply.github.com>
2024-02-29 15:54:08 +0530

    create loading page (#44)
    * Modify and link login page
    
    * create register page
    
    * link register and signup page
    
    * modify login page
    
    * sign in flutter
    
    * connect forgetpw
    
    * change colors
    
    * add social media icons in sign in
    
    * changes
    
    * signup page flutter
    
    * connect privacy page to signup page
    
    * register page Firebase
    
    * do changes firebase
    
    * create loading page
    
    * add firebase console
    
    * connect pages
    
    * Connect pages
    
    * create loading page
    
    ---------
    
    Co-authored-by: Williyam Silvage Sachini Dishala <90495152+desi98@users.noreply.github.com>

--- lib/Pages/loadingPage.dart
+++ lib/Pages/loadingPage.dart
@@ -1,7 +1,9 @@
+import 'package:auto_play/Pages/get_start.dart';
+import 'package:auto_play/Pages/home.dart';
 import 'package:flutter/material.dart';
 
 class Loading extends StatefulWidget {
-  const Loading({super.key});
+   const Loading({super.key});
 
   @override
   State<Loading> createState() => _LoadingState();
@@ -11,22 +13,52 @@ class _LoadingState extends State<Loading> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
+      
+      backgroundColor: Colors.white,
+      body: Stack( // Using Stack to position the profile photo
         children: [
-          Container(
-            decoration: const BoxDecoration(
-              gradient: LinearGradient(
-                begin: Alignment.topCenter,
-                end: Alignment.bottomCenter,
-                colors: [
-                  Colors.white,
-                  Colors.blue,
-                ],
-              ),
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Image.asset(
+                  'assets/Group_6.png',
+                  height: 130,
+                ),
+                SizedBox(height: 150),
+                ElevatedButton(
+                  onPressed: () {
+                     Navigator.push(context, MaterialPageRoute(builder: (context) => const GetStart()));
+                  },
+                  child: Text('Ready to start with AutoBeats',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold
+                  ),),
+                  style: ElevatedButton.styleFrom(
+                    padding:
+                        EdgeInsets.symmetric(horizontal: 20, vertical: 30),
+                    primary: const Color.fromARGB(255, 4, 21, 35),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                      side: BorderSide(color: Colors.lightBlue, width: 2),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Positioned(
+            top: 10,
+            right: 10,
+            child: CircleAvatar(
+              radius: 30,
+             // Placeholder for user profile photo
             ),
           ),
         ],
-    ),
+      ),
     );
   }
 }
